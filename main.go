9859c772339512d7c8a2e792c3b2584e00eb3ced
vir$i <egrvrb@gmail.com>
2025-06-02 20:29:30 +0300

    init logger

--- main.go
+++ main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/virsi/fileConverter/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDevelopment = "development"
+	envProduction = "production"
+	envTest = "test"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Print(cfg)
 
-	// TODO init logger
+	log := setupLogger(cfg.Env)
+	log.Info("starting file converter service", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
 	// TODO init storage
 	// TODO init router
 	// TODO start server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDevelopment:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProduction:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
