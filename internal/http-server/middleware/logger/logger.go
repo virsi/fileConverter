3a37c07ac7c8920322d2603351e535b25f46e563
vir$i <egrvrb@gmail.com>
2025-06-07 11:35:54 +0300

    init router

--- internal/http-server/middleware/logger/logger.go
+++ internal/http-server/middleware/logger/logger.go
@@ -1,4 +1,4 @@
-package logger
+package mwlogger
 
 import (
 	"net/http"
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/virsi/fileConverter/internal/config"
+	"github.com/virsi/fileConverter/internal/http-server/middleware/logger"
 	"github.com/virsi/fileConverter/internal/storage/sqlite"
 )
 
@@ -17,19 +20,32 @@ const (
 )
 
 func main() {
+	// Load configuration
 	cfg := config.MustLoad()
 	fmt.Print(cfg)
 
+	// Setup logger based on environment
+	// TODO: setup a pretty logger
 	log := setupLogger(cfg.Env)
 	log.Info("starting file converter service", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
 
+	// Initialize storage
 	storage, err := sqlite.NewStorage(cfg.StoragePath)
 	if err != nil {
 		log.Error("Storage is not init", slog.String("error", err.Error())) // TODO: create err func in internal/lib/logger/slog -- slog.Attr{key: error, value: err.Error()} slog.StringValue(err.Error())
 		os.Exit(1)
 	}
 	_ = storage
+
+	// Initialize router
+	// TODO: add cors middleware
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat) // works only with chi
+	router.Use(mwlogger.New(log))
 
 	// GetFileByID test
 	// file, err := storage.GetFileByID(1)
@@ -47,7 +63,6 @@ func main() {
 	// }
 	// log.Info("File status updated", slog.Int64("id", 1), slog.String("status", "processing"))
 
-	// TODO init router
 	// TODO start server
 }
 
