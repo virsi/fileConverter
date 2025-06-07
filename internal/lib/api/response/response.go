57c3ac8883d87e2b9ce04607857975c90a1728ad
vir$i <egrvrb@gmail.com>
2025-06-07 17:50:27 +0300

    [lib/api/response] add ValidationError

--- internal/lib/api/response/response.go
+++ internal/lib/api/response/response.go
@@ -1,5 +1,12 @@
 package response
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
 type Response struct {
 	Status string `json:"status"`
 	Error  string `json:"error,omitempty"`
@@ -46,5 +53,22 @@ func InternalServerError(err string) Response {
 	return Response{
 		Status: StatusInternalServerError,
 		Error:  err,
+	}
+}
+
+func ValidationError(errs validator.ValidationErrors) Response {
+	var errMsgs []string
+
+	for _, err := range errs {
+		switch err.ActualTag() {
+		case "required":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not required field", err.Field()))
+		default:
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not valid", err.Field()))
+		}
+	}
+	return Response{
+		Status: StatusError,
+		Error: strings.Join(errMsgs, ",  "),
 	}
 }
